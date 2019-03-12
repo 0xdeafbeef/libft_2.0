efdd6a8fe7fac5db01c32b4848fab0af2bfd294b
rusmonkey <rusmonkey@users.noreply.github.com>
2019-03-12 17:18:49 +0300

    📦 NEW: size_t itoa

--- ft_itoa_size_t.c
+++ ft_itoa_size_t.c
@@ -11,13 +11,10 @@
 /* ************************************************************************** */
 
 #include <libft.h>
-#include <printf.h>
 
-#include <libft.h>
-
-static size_t	get_str_len(int n)
+static size_t get_str_len(size_t n)
 {
-	size_t		i;
+	size_t i;
 
 	i = 1;
 	while (n /= 10)
@@ -25,25 +22,19 @@ static size_t	get_str_len(int n)
 	return (i);
 }
 
-char			*ft_itoa(int n)
+char *ft_itoa_big(size_t n)
 {
-	char			*str;
-	size_t			str_len;
-	unsigned int	n_cpy;
+	char *str;
+	size_t str_len;
+	size_t n_cpy;
 
 	str_len = get_str_len(n);
 	n_cpy = n;
-	if (n < 0)
-	{
-		n_cpy = -n;
-		str_len++;
-	}
+
 	if (!(str = ft_strnew(str_len)))
 		return (NULL);
 	str[--str_len] = n_cpy % 10 + '0';
 	while (n_cpy /= 10)
 		str[--str_len] = n_cpy % 10 + '0';
-	if (n < 0)
-		*(str + 0) = '-';
 	return (str);
 }
